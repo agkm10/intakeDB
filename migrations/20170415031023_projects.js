0ed226ba09628afa0ea976ed65e3b51f9eb931ca
Ben Biggs <bfletcherbiggs@gmail.com>
2017-04-15 16:23:37 -0500

    projects table added

--- migrations/20170415031023_projects.js
+++ migrations/20170415031023_projects.js
@@ -1,14 +1,12 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('components', function(t) {
+  return knex.schema.createTable('projects', function(t) {
         t.increments('id').unsigned().primary().notNull();
         t.timestamps();
-        t.integer('user_id').references('components.id').notNull().onDelete('cascade');
-        t.string('title').notNull();
-        t.string('data').notNull();
-        t.string('project_id').notNull().onDelete('cascade');
+        t.integer('user_id').references('users.id').notNull().onDelete('cascade');
+        t.unique(['id', 'user_id']);
     });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('components');
+  return knex.schema.dropTable('projects');
 };
