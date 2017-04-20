f477eae12e3c0771de644eb6431565af659f264c
Ben Biggs <bfletcherbiggs@gmail.com>
2017-04-19 23:43:20 -0500

    added chats

--- migrations/20170419233557_chats.js
+++ migrations/20170419233557_chats.js
@@ -1,13 +1,14 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('messages', function(t) {
+  return knex.schema.createTable('chats', function(t) {
         t.increments('id').unsigned().primary().notNull();
         t.timestamps(true,true);
         t.integer('user_id').references('users.id').notNull().onDelete('cascade');
-        t.string('message').notNull();
+        t.integer('admin_id').references('admins.id').notNull().onDelete('cascade');
+        t.unique(['user_id', 'admin_id']);
         t.string('type').notNull();
     });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('messages');
+  return knex.schema.dropTable('chats');
 };
---
+++ migrations/201788150249907_messages.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('messages', function(t) {
+        t.increments('id').unsigned().primary().notNull();
+        t.timestamps(true,true);
+        t.integer('chat_id').references('chats.id').notNull().onDelete('cascade');
+        t.integer('user_id').references('users.id').notNull().onDelete('cascade');
+        t.string('message').notNull();
+        t.string('type').notNull();
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('messages');
+};
