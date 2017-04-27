a99c599bd2068a3ad0d866913393245edf12d19c
Ben Biggs <bfletcherbiggs@gmail.com>
2017-04-27 16:55:59 -0500

    kurt changes

--- migrations/20170425160610_userdata.js
+++ migrations/20170425160610_userdata.js
@@ -1,35 +1,44 @@
 
-exports.up = function(knex, Promise) {
-    return knex.schema.createTable('userdata', function(t) {
-        t.increments('id').unsigned().primary().notNull();
-        t.integer('user_id').references('users.id').notNull().onDelete('cascade');
-        t.timestamps(true,true);
-        t.json('websites');
-        t.string('billingpoc').defaultTo('');
-        t.string('billingphonenumber').defaultTo('');
-        t.string('billingemail').defaultTo('');
-        t.string('billingadd').defaultTo('');
-        t.string('billingcity').defaultTo('');
-        t.string('billingstate').defaultTo('');
-        t.integer('billingzip').defaultTo(0);
-        t.string('businessname').defaultTo('');
-        t.string('businessadd').defaultTo('');
-        t.string('businesscity').defaultTo('');
-        t.string('businessstate').defaultTo('');
-        t.string('businessemail').defaultTo('');
-        t.string('designwho').defaultTo('');
-        t.string('designaction').defaultTo('');
-        t.string('designurl').defaultTo('');
-        t.string('socialfacebook').defaultTo('');
-        t.string('socialinstagram').defaultTo('');
-        t.string('socialtwitter').defaultTo('');
-        t.string('socialyoutube').defaultTo('');
-        t.string('sociallinkedin').defaultTo('');
-        t.string('socialpinterest').defaultTo('');
-        t.string('socialother').defaultTo('');
+exports.up = ( knex, Promise ) => {
+    return knex.schema.createTable( 'userdata', ( t ) => {
+        t.increments( 'id' ).unsigned().primary().notNull();
+        t.integer( 'user_id' ).references( 'users.id' ).notNull().onDelete( 'cascade' );
+        t.timestamps( true, true );
+        t.json( 'websites' ).defaultTo( '[{"text1":"","text2":"","index":0}]' );
+        t.string( 'billingpoc' ).defaultTo( ' ' );
+        t.string( 'billingphonenumber' ).defaultTo( '' );
+        t.string( 'billingemail' ).defaultTo( ' ' );
+        t.string( 'billingadd' ).defaultTo( ' ' );
+        t.string( 'billingcity' ).defaultTo( ' ' );
+        t.string( 'billingstate' ).defaultTo( ' ' );
+        t.string( 'billingzip' ).defaultTo( ' ' );
+        t.string( 'businessname' ).defaultTo( ' ' );
+        t.string( 'businessadd' ).defaultTo( ' ' );
+        t.string( 'businesscity' ).defaultTo( ' ' );
+        t.string( 'businessstate' ).defaultTo( ' ' );
+        t.string( 'businessemail' ).defaultTo( ' ' );
+        t.string( 'designwho' ).defaultTo( ' ' );
+        t.string( 'designaction' ).defaultTo( ' ' );
+        t.string( 'designurl' ).defaultTo( ' ' );
+        t.string( 'socialfacebook' ).defaultTo( ' ' );
+        t.string( 'socialinstagram' ).defaultTo( ' ' );
+        t.string( 'socialtwitter' ).defaultTo( ' ' );
+        t.string( 'socialyoutube' ).defaultTo( ' ' );
+        t.string( 'sociallinkedin' ).defaultTo( ' ' );
+        t.string( 'socialpinterest' ).defaultTo( ' ' );
+        t.string( 'socialother' ).defaultTo( ' ' );
+        t.string( 'company' ).defaultTo( ' ' );
+        t.string( 'firstname' ).defaultTo( ' ' );
+        t.string( 'lastname' ).defaultTo( ' ' );
+    })
+    .then( () => {
+        return knex( "userdata" ).insert( [
+            { user_id: 1 },
+            { user_id: 2 }
+        ])
     })
 };
 
-exports.down = function(knex, Promise) {
-      return knex.schema.dropTable('userdata');
+exports.down = ( knex, Promise ) => {
+      return knex.schema.dropTable( 'userdata' );
 };
